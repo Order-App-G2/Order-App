e4a873ee4f7c410afd14e35a1bb7734969c70b32
danielkotev <danielkotev@gmail.com>
2021-12-08 10:26:48 +0200

    create CORS #35

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0fed4204af728cc75af855673b710ce4'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db' //for testing
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2865428@localhost/Orders'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:11332255@localhost/Orders'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+CORS(app)
+cors = CORS(app, resource={
+    r"/*": {
+        "origins": "*"
+    }
+})
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
